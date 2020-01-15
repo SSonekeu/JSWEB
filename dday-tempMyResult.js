ba7838327068b24161e4ac742110bea67a2e35cd
SSonekeu <50897241+SSonekeu@users.noreply.github.com>
2020-01-15 15:22:56 +0900

    Add files via upload

---
+++ dday-tempMyResult.js
@@ -0,0 +1,7 @@
+﻿var now = new Date();
+var SooneungDay = new Date("2020-11-19");
+var toNow = now.getTime();
+var toSD = SooneungDay.getTime();
+var Time = toSD - toNow;
+var Future = Math.round(Time/(1000 * 60 * 60 * 24));
+document.querySelector('#accent').innerText = Future + "일"
