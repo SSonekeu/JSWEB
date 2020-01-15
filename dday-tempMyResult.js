d1cdd540451591d6bf49886eb8f3f1fa6d3aaf9f
woojin <50897241+SSonekeu@users.noreply.github.com>
2020-01-16 00:54:38 +0900

    Add files via upload

--- dday-tempMyResult.js
+++ dday-tempMyResult.js
@@ -1,4 +1,4 @@
-﻿var now = new Date();
+var now = new Date();
 var SooneungDay = new Date("2020-11-19");
 var toNow = now.getTime();
 var toSD = SooneungDay.getTime();
@@ -6,32 +6,19 @@ var Time = toSD - toNow;
 var Future = Math.round(Time/(1000 * 60 * 60 * 24));
 document.querySelector('#accent').innerText = Future + "일";
 
-calcDay(100);
-calcDay(200);
-calcDay(365);
+calcDate(100);
+calcDate(200);
+calcDate(365);
 
-function calcDay(ChooseDay) 
-{
-    if(Future - ChooseDay <= 0)
-    {
+function calcDate(ChooseDay) {
+    if(Future - ChooseDay <= 0) {
         document.querySelector("#date" + ChooseDay).innerText = "이미 지남 ㅅㄱ";
         return;
     }
-    var i=1;
-    while(Future - i != ChooseDay)
-    {
-        i++;
-    }
-    calcDate(i, ChooseDay);
-}
-
-function calcDate(day, ChooseDay) {
-   var future = toSD - day * (1000 * 60 * 60 * 24);
-   document.write(toSD + " - " + day * (1000 * 60 * 60 * 24) + " = " + future + "<br>");
-   var Fday = new Date(future);
-   document.write(Fday + "<br>");
-   var year = Fday.getFullYear();
-   var month = Fday.getMonth();
-   var day = Fday.getDate();
-   document.querySelector("#date" + ChooseDay).innerText = year + "-" + (month+1) + "-" + day;
+    var future = toSD - ChooseDay * (1000 * 60 * 60 * 24);
+    var Fday = new Date(future);
+    var year = Fday.getFullYear();
+    var month = Fday.getMonth();
+    var day = Fday.getDate();
+    document.querySelector("#date" + ChooseDay).innerText = year + "-" + (month+1) + "-" + day;
 }
